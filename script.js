9457c6d2f56dbdd2471e36fb1a9384aee5b2e6ff
EvergreenTh <maussiontom@gmail.com>
2026-06-05 02:59:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// CHANGE SUN MOON ICON + CHANGE MENU BARS COLOR + LOGO
+const icon = document.getElementById("theme_icon");
+const iconbars = document.getElementById("openSidebar");
+const logo = document.getElementById("logo");
+
+icon.addEventListener("click", () => {
+	if (icon.src.includes("moon.svg")) {
+		icon.src = "IMG/UI/sun.svg";
+		iconbars.src = "IMG/UI/menu_bars_sun.svg";
+		logo.src = "IMG/UI/logo_sun.svg";
+	} else {
+		icon.src = "IMG/UI/moon.svg";
+		iconbars.src = "IMG/UI/menu_bars_moon.svg";
+		logo.src = "IMG/UI/logo_moon.svg";
+	}
+});
+
+
+
+
+
+
+
+// CHANGE PAGE THEME
+const toggleBtn = document.getElementById("theme_icon");
+
+toggleBtn.addEventListener("click", () => {
+	const currentTheme = document.documentElement.getAttribute("data-theme");
+
+	if (currentTheme === "dark") {
+		document.documentElement.removeAttribute("data-theme");
+		localStorage.setItem("theme", "light");
+	} else {
+		document.documentElement.setAttribute("data-theme", "dark");
+		localStorage.setItem("theme", "dark");
+	}
+});
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.documentElement.setAttribute("data-theme", "dark");
+
+    icon.src = "IMG/UI/moon.svg";
+    iconbars.src = "IMG/UI/menu_bars_moon.svg";
+	logo.src = "IMG/UI/logo_moon.svg";
+} else {
+    document.documentElement.setAttribute("data-theme", "light");
+
+    icon.src = "IMG/UI/sun.svg";
+    iconbars.src = "IMG/UI/menu_bars_sun.svg";
+	logo.src = "IMG/UI/logo_sun.svg";
+}
+
+
+
+
+
+
+
+
+
+// SIDEBAR
+const openBtn = document.getElementById("openSidebar");
+const closeBtn = document.getElementById("closeSidebar");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+openBtn.addEventListener("click", () => {
+	sidebar.classList.add("active");
+	overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+	sidebar.classList.remove("active");
+	overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+	sidebar.classList.remove("active");
+	overlay.classList.remove("active");
+});
+
+
+
+
+
+
+// CHANGE BACKGROUND IMAGE ON NOV 18TH
+// const now = new Date();
+
+// const isNov18 = now.getDate() === 18 && now.getMonth() === 10;
+
+// if (isNov18) {
+// 	document.getElementById("main").style.backgroundImage = 'url("IMG/18nov.jpg")';
+// }
